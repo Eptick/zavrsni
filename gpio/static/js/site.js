40a2c1e30bba43e2ed9562cb46866b8e95ee5e22
Leon <redzicleon@gmail.com>
2017-12-30 11:56:03 +0100

    Vue.js implementation and created a single board component

--- gpio/static/js/site.js
+++ gpio/static/js/site.js
@@ -1,15 +1,71 @@
 var socket = io();
 socket.on('status update', function(msg){
-
-});
-$(document).ready(() => {
-    // $.post( "/", { action: "status" }, function( data ) {});
-    function init() {
     
+});
+Vue.config.devtools = true
+$(document).ready(() => {
+Vue.component('board', {
+    template:
+    `
+    <div class="board" :id="getBoardId()">
+        <div class="row" v-for="(row, rowIndex) in leds">
+            <div class="led" v-for="(led, ledIndex) in row">
+            <input type="checkbox" v-bind:name="getLedId(rowIndex,ledIndex)" v-bind:id="getLedId(rowIndex,ledIndex)"><label v-bind:for="getLedId(rowIndex,ledIndex)"></label>
+            </div>
+        </div>
+    </div>
+    `,
+    methods: {
+        getLedId: function(row, led) {
+            return this.frame +'-'+row+led; 
+        },
+        getBoardId: function() {
+            return 'board-'+this.frame; 
+        }
+    },
+    data: () => {
+    return {frame: 1,
+        leds : [
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0]]
+        }
     }
-    // $("#stop").click(() => {
-    //     $.post( "/", { action: "stop" }, function( data ) {
-    //         console.log( data );
-    //     });
-    // });
+})
+window.gpio = new Vue({
+    el: '#app',
+    data: {
+        frames: 1,
+        message: 'Hello Vue!'
+    },
+    methods: {
+        getFrames: function() {
+            let data = [];
+            $("#board-1 .row").each(function () {
+                let row = []
+                $(this).children(".led").each(function () {
+                    let led = $(this).children("input[type='checkbox']").first()
+                    let state = (led.is(":checked")===true)?1:0;
+                    row.push(state)
+                })
+                data.push(row)
+            })
+            console.log(data);
+        }
+    }
+})
+// // $.post( "/", { action: "status" }, function( data ) {});
+// function init() {
+
+// }
+// // $("#stop").click(() => {
+// //     $.post( "/", { action: "stop" }, function( data ) {
+// //         console.log( data );
+// //     });
+// // });
 })
