61ec32375d19e72487ddae1edeb80b030733792f
Leon <redzicleon@gmail.com>
2018-01-13 16:57:19 +0100

    front end done

--- gpio/static/js/site.js
+++ gpio/static/js/site.js
@@ -1,66 +1,30 @@
 var socket = io();
-socket.on('status update', function(msg){
-    
-});
-Vue.config.devtools = true
-window.framesData = [];
-$(document).ready(() => {
+socket.on('status update', function(msg){});
+Vue.config.devtools = true;
+function countProperties(obj) {
+    var count = 0;
 
-Vue.component('board', {
-    props: {
-        frame: {
-            default: 1
-        }
-    },
-    template:
-    `
-    <div class="board" :id="getBoardId()">
-        <div class="row" v-for="(row, rowIndex) in getFrame()">
-            <div class="led" v-for="(led, ledIndex) in row">
-            <input type="checkbox" v-model="getFrame()[rowIndex][ledIndex]" :true-value="1" :false-value="0" v-bind:name="getLedId(rowIndex,ledIndex)" v-bind:id="getLedId(rowIndex,ledIndex)"><label v-bind:for="getLedId(rowIndex,ledIndex)"></label>
-            </div>
-        </div>
-    </div>
-    `,
-    methods: {
-        getFrame: function() {
-            if(window.framesData !== undefined)
-                return window.framesData[this.frame-1]
-            return []
-        },
-        getLedId: function(row, led) {
-            return this.frame +'-'+row+led; 
-        },
-        getBoardId: function() {
-            return 'board-'+this.frame; 
-        }
-    },
-    data: () => {
-    return {
-        leds : [
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0]]
-        }
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            ++count;
     }
-})
+
+    return count;
+}
+$(document).ready(function () {
 window.gpio = new Vue({
     el: '#app',
     data: {
-        frames: 4,
-        message: 'Hello Vue!'
+        x: 8,
+        y: 8,
+        frame: [],
+        frames: 2,
+        message: 'Hello Vue!',
+        currentIndex: 0,
+        show: true,
+        framesData: {}
     },
     methods: {
-        getFramesData: function() {
-            if(window.framesData !== undefined)
-                return window.framesData
-            return []
-        },
         getFrames: function() {
             let data = [];
             $("#board-1 .row").each(function () {
@@ -73,20 +37,53 @@ window.gpio = new Vue({
                 data.push(row)
             })
             console.log(data);
+        },
+        addFrame: function() {
+            let row = [];
+            let frame = [];
+            for(let i = 0; i < this.x; i++)
+                row.push(0)
+            for(let i = 0; i < this.y; i++)
+                frame.push(row.slice(0))
+            Vue.set(this.framesData, window.countProperties(this.framesData), frame.slice(0))
+        },
+        getIndex: function() {
+            return this.$slideshowBoards.index;
+        },
+        refreshSlideshow() {
+            this.show = false;
+            this.show = true;
+        },
+        getLedId: function(index, row, led) {
+            return index +'-'+row+led; 
+        },
+        setLedState(index, row, led) {
+            let state = this.framesData[parseInt(index)][row][led];
+            if(state === 0)
+                this.framesData[parseInt(index)][row][led] = 1;
+            else
+                this.framesData[parseInt(index)][row][led] = 0;
+            this.refreshSlideshow()
+        },
+        ledIsActive(index, rowIndex, ledIndex) {
+            let state = this.framesData[parseInt(index)][rowIndex][ledIndex];
+            return (state === 0)?false:true;
         }
     },
     created: function () {
-        window.framesData = []
-        let x = 8;
-        let y = 8;
-        let frame = []
-        let row = [];
-        for(let i = 0; i < x; i++)
-            row.push(0)
-        for(let i = 0; i < y; i++)
-            frame.push(row)
-        for(let i = 0; i < this.frames; i++)
-            window.framesData.push(frame)
+        this.framesData = {}
+        for(let j = 0; j < this.frames; j++) {
+            let row = [];
+            let frame = [];
+            for(let i = 0; i < this.x; i++) {
+                row.push(0)
+            }
+            for(let i = 0; i < this.y; i++) {
+                frame.push(row.slice(0))
+            }
+            this.framesData[j] = frame;
+        }
+        this.refreshSlideshow()
     }
 })
 // // $.post( "/", { action: "status" }, function( data ) {});
