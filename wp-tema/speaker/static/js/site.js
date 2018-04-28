ff17a5ab3734dc14b339d35e1373dad004e5c459
Leon <leon@euroart93.hr>
2018-04-28 18:52:26 +0200

    a

--- wp-tema/speaker/static/js/site.js
+++ wp-tema/speaker/static/js/site.js
@@ -1,6 +1,6 @@
 
 $(document).ready(() => {
-    var socket = io(ajaxurldata.link);
+    var socket = io(ajaxurldata.link + "/");
     socket.on('status update', function(msg){
         console.log(msg);
         switch(msg.status) {
