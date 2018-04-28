b512e2a8a9d181ca65d25229bb4b88bcd157b0f7
Eptick <redzicleon@gmail.com>
2018-04-28 14:05:10 +0000

    a

--- wp-tema/functions.php
+++ wp-tema/functions.php
@@ -7,8 +7,17 @@ function zavrsni_enqueue() {
     $vars = array(
         'dir' => get_template_directory_uri()
     );
+
+    if(is_page()){
+        global $wp_query;
+            $template_name = get_post_meta( $wp_query->post->ID, '_wp_page_template', true );
+            if($template_name == 'tpl-gpoi.php'){
+                wp_enqueue_script('gpio-main', get_template_directory_uri() .'/gpio/static/js/site.js');
+            }
+       }
+
+
     wp_localize_script( 'main', 'template', $vars );
     wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main.js' );
 }
 add_action( 'wp_enqueue_scripts', 'zavrsni_enqueue' );
-?>
