1c4c64a4a7ea95d66c2f6092d0ce6710e58450ac
Leon <leon@euroart93.hr>
2018-04-28 17:08:03 +0200

    aaa

--- wp-tema/index.php
+++ wp-tema/index.php
@@ -4,4 +4,17 @@
 <h4>Uspiješno je <span><i>podignut</i></span></h4>
 <p>By: Leon Redžić</p>
 
+
+<div class="pages-wrapper">
+    <a href="<?= get_field('gpio_page'); ?>">
+        <div class="gpio">
+            gpio
+        </div>
+    </a>
+    <a href="<?= get_field('speakers_page'); ?>">
+        <div class="speaker">
+            SPEAKERS
+        </div>
+    </a>
+</div>
 <?php get_footer(); ?>
