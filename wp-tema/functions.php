1efc8e2354da7089af3a1c11770ce89037544585
Leon <leon@euroart93.hr>
2018-04-28 16:27:00 +0200

    aaaa

--- wp-tema/functions.php
+++ wp-tema/functions.php
@@ -3,21 +3,24 @@ function zavrsni_enqueue() {
     wp_enqueue_style( 'reset', get_template_directory_uri() . '/reset.css' );
     wp_enqueue_style( 'style-name', get_stylesheet_uri() );
     wp_enqueue_script( 'jquery' );
-    wp_register_script( 'main', get_template_directory_uri() . '/js/main.js' );
-    $vars = array(
-        'dir' => get_template_directory_uri()
-    );
+
+    wp_enqueue_style( 'ui-kit', get_template_directory_uri() . 'gpio/static/uikit/css/uikit.min.css');
+    wp_enqueue_script('common-socket', get_template_directory_uri() .'/gpio/static/socket/socket.io.slim.js');
+    wp_enqueue_script('common-uikit', get_template_directory_uri() .'/gpio/static/uikit/js/uikit.min.js');
+    wp_enqueue_script('common-uikit-icons', get_template_directory_uri() .'/gpio/static/uikit/js/uikit-icons.min.js');
 
     if(is_page()){
         global $wp_query;
             $template_name = get_post_meta( $wp_query->post->ID, '_wp_page_template', true );
             if($template_name == 'tpl-gpoi.php'){
+                wp_enqueue_script('gpio-ve', 'https://unpkg.com/vue');
+                wp_enqueue_style( 'gpio-css', get_template_directory_uri() . '/gpio/static/css/style.css' );
                 wp_enqueue_script('gpio-main', get_template_directory_uri() .'/gpio/static/js/site.js');
             }
+            if($template_name == 'tpl-speaker.php'){
+                wp_enqueue_style( 'speaker-css', get_template_directory_uri() . '/speaker/static/css/style.css' );
+                wp_enqueue_script('speaker-main', get_template_directory_uri() .'/speaker/static/js/site.js');
+            }
        }
-
-
-    wp_localize_script( 'main', 'template', $vars );
-    wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main.js' );
 }
 add_action( 'wp_enqueue_scripts', 'zavrsni_enqueue' );
---
+++ wp-tema/tpl-speaker.php
@@ -0,0 +1,15 @@
+<div class="search">
+    <form class="uk-search uk-search-large" id="search">
+        <span uk-search-icon></span>
+        <input class="uk-search-input" type="search" placeholder="Search" id="search-input">
+    </form>
+</div>
+<div class="search-results">
+    <div class="spinner" uk-spinner></div>
+    <ul class="uk-list uk-list-striped" id="search-results">
+    </ul>
+</div>
+<div id="player-status">
+    <p id="status" class="uk-margin-remove">...</p>
+    <button id="stop"class="uk-button uk-button-secondary"><span uk-icon="icon: close"></span></button>
+</div>
